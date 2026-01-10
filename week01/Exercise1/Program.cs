760e75d7a976794c048837738be0fc9a4726c089
Tom Emem <tomemem361@gmail.com>
2026-01-10 16:16:16 +0100

    I adjusted the code to ask for  first name and last name and display it back like James Bond inroduced himself

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("My name is Tom Emem.");
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
     }
 }
