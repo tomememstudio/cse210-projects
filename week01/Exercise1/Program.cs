923051d7ce82992ac4a268c2b0bebad44b31afac
Tom Emem <tomemem361@gmail.com>
2026-01-10 16:48:48 +0100

    Adjusted the code to ask for a grade percentage and caculate the grade goten between A, B, C, D, E or F.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,6 +8,6 @@ class Program
         string firstname = Console.ReadLine();
         Console.Write("What is your last name? ");
         string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}");
     }
 }
