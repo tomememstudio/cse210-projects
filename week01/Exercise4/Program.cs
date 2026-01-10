5d1e98ec1f5c635dd215d910dda2c73949673950
Tom Emem <tomemem361@gmail.com>
2026-01-10 17:16:41 +0100

    a program that collects numbers until 0 is givem. It adds it up, averages it and gives the largest number back.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+        int sum = 0;
+        int largest = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            sum += num;
+
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+        double average = (double)sum / numbers.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
