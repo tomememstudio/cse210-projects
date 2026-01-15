77418506099037e5831ff2c45ab07060b9f7e82d
Tom Emem <tomemem361@gmail.com>
2026-01-15 13:49:47 +0100

    Add Job class with member variables and display method Add Resume class with job list and display method
    Update Program to create resume and display job history

---
+++ week02/Resumes/job.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public string _startYear;
+    public string _endYear;
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+
+}
