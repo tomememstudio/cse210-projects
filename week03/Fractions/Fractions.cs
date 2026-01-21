6baf6b79f82e10f63d65032ad203df5e14961bbb
Tom Emem <tomemem361@gmail.com>
2026-01-21 16:27:02 +0100

    Fix fraction initialization to use correct constructors Add single-parameter constructor to Fraction class

--- week03/Fractions/Fractions.cs
+++ week03/Fractions/Fractions.cs
@@ -11,6 +11,12 @@ public class Fraction
         _top = 1;
         _bottom = 1;
     }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
     public Fraction(int top, int bottom)
     {
         _top = top;
