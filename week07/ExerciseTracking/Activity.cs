1aa33b6633df1cbcb859b2e5978a6a84599d83d3
Tom Emem <tomemem361@gmail.com>
2026-02-19 21:56:00 +0100

    git init git add . git commit -m "Initial commit - W07 Exercise Tracking" git branch -M main git remote add origin https://github.com/YOURUSERNAME/REPO-NAME.git git push -u origin main

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public string GetDate() => _date;
+    public int GetMinutes() => _minutes;
+
+    public abstract double GetDistance(); // km
+    public abstract double GetSpeed();    // kph
+    public abstract double GetPace();     // min per km
+
+    public string GetSummary()
+    {
+        return $"{_date} {this.GetType().Name} ({_minutes} min): " +
+               $"Distance {GetDistance():0.0} km, " +
+               $"Speed {GetSpeed():0.0} kph, " +
+               $"Pace {GetPace():0.00} min per km";
+    }
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>
+        {
+            new Running("03 Nov 2022", 30, 4.8),
+            new Cycling("04 Nov 2022", 45, 20.0),
+            new Swimming("05 Nov 2022", 40, 30)
+        };
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
